1bbe80db49ae01647e60410f2d3dd39bedcbbf27
Luzyla <63380618+Luzyla@users.noreply.github.com>
2020-06-24 23:45:00 -0300

    Add files via upload

---
+++ effects.js
@@ -0,0 +1,7 @@
+document.getElementById("modo-oscuro").onclick = () => {
+    document.body.classList.add("modo-oscuro")
+}
+
+document.getElementById("modo-claro").onclick = () => {
+    document.body.classList.remove("modo-oscuro")
+}
